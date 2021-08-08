34e0478f5a167fcc02b8d63954e26d6d8c6838e6
Kirill <abramovki96@mail.ru>
2021-08-09 02:39:08 +0300

    commit after new tests

--- homework.py
+++ homework.py
@@ -1,6 +1,5 @@
 import os
 import time
-
 import logging
 import requests
 import telegram
@@ -13,18 +12,19 @@ PRAKTIKUM_TOKEN = os.getenv("PRAKTIKUM_TOKEN")
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
+bot = telegram.Bot(token=TELEGRAM_TOKEN)
+
 
 def parse_homework_status(homework):
     homework_name = homework['homework_name']
     if homework['status'] == 'rejected':
-        verdict = 'К сожалению в работе нашлись ошибки.'
-    else:
-        verdict = 'Ревьюеру всё понравилось, '\
-            'можно приступать к следующему уроку.'
+        verdict = 'К сожалению, в работе нашлись ошибки.'
+    elif homework['status'] == 'approved':
+        verdict = 'Ревьюеру всё понравилось, работа зачтена!'
     return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
 
 
-def get_homework_statuses(current_timestamp):
+def get_homeworks(current_timestamp):
     url = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
     headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
     payload = {'from_date': current_timestamp}
@@ -32,31 +32,28 @@ def get_homework_statuses(current_timestamp):
     return homework_statuses.json()
 
 
-def send_message(message, bot_client):
+def send_message(message):
     logging.info('Новое сообщение!')
-    return bot_client.send_message(chat_id=CHAT_ID, text=message)
+    return bot.send_message(chat_id=CHAT_ID, text=message)
 
 
 def main():
-    # проинициализировать бота здесь
-    bot = telegram.Bot(token=TELEGRAM_TOKEN)
     logging.debug('Поехали!')
     current_timestamp = int(time.time())  # начальное значение timestamp
 
     while True:
         try:
-            new_homework = get_homework_statuses(current_timestamp)
+            new_homework = get_homeworks(current_timestamp)
             if new_homework.get('homeworks'):
                 try:
                     send_message(
                         parse_homework_status(
                             new_homework.get('homeworks')[0]
-                        ),
-                        bot
+                        )
                     )
                 except IndexError:
                     logging.error('Такую домашку ты не делал!')
-                    send_message('Такую домашку ты не делал', bot)
+                    send_message('Такую домашку ты не делал')
             current_timestamp = new_homework.get(
                 'current_date',
                 current_timestamp
@@ -64,7 +61,7 @@ def main():
             time.sleep(1200)
 
         except Exception as e:
-            print(f'Бот столкнулся с ошибкой: {e}')
+            print(f'Бот упал с ошибкой: {e}')
             time.sleep(5)
 
 
@@ -76,6 +73,4 @@ logging.basicConfig(
 
 
 if __name__ == '__main__':
-    # main()
-    port = int(os.environ.get('PORT', 5000))
-    main.run(host='0.0.0.0', port=port)
+    main()
